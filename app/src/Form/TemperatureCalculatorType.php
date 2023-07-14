9ea75b4ec4128e788044122901635144743858f4
mwojcikbs <mwojcikbs@gmail.com>
2023-07-14 11:30:12 +0200

    add form validation

--- app/src/Form/TemperatureCalculatorType.php
+++ app/src/Form/TemperatureCalculatorType.php
@@ -7,16 +7,28 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class TemperatureCalculatorType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('city')
-            ->add('country')
-            ->add('averageTemperature', TextType::class, array('required' => false))
-        ;
+            ->add('city', TextType::class, [
+                'attr' => ['maxlength' => 255],
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['max' => 255])
+                ]
+            ])
+            ->add('country', TextType::class, [
+                'attr' => ['maxlength' => 255],
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['max' => 255])
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
